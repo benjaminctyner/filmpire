5b21f81d42163cfc4102770dae9f1290b912fc93
Ben <benjaminctyner@gmail.com>
2023-04-15 21:51:25 -0600

    day1.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import Box from '@mui/material/Box';
+// import { alpha } from '@mui/material/styles';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './';
+// import useStyles from './styles';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Box
+        className='root'
+        sx={{
+          display: 'flex',
+          height: '100%',
+        }}
+      >
+        <CssBaseline />
+        <NavBar />
+        <Box
+          className='content'
+          sx={{
+            flexGrow: 1,
+            padding: '2em',
+          }}
+        >
+          <Box
+            className='toolbar'
+            sx={{
+              height: '70px',
+            }}
+          />
+          <Routes>
+            <Route exact path='/movie/:id' element={<MovieInformation />} />
+            <Route exact path='/actors/:id' element={<Actors />} />
+            <Route exact path='/' element={<Movies />} />
+            <Route exact path='/profile/:id' element={<Profile />} />
+          </Routes>
+        </Box>
+      </Box>
+    </Router>
+  );
 };
 
 export default App;
